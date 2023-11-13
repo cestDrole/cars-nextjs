ee608875ba26887b60ae9904355357653f2d7d03
SJ <smwjasnosz@gmail.com>
2023-11-13 21:30:25 +0100

    Add CarCard component, add cars API call

--- components/index.ts
+++ components/index.ts
@@ -2,9 +2,10 @@ import Hero from './Hero'
 import CustomButton from './CustomButton'
 import Navbar from './Navbar'
 import Footer from './Footer'
-import SearchBar from './Searchbar'
+import SearchBar from './SearchBar'
 import CustomFilter from './CustomFilter'
 import SearchManufacturer from './SearchManufacturer'
+import CarCard from './CarCard'
 
 export {
   Hero,
@@ -13,5 +14,6 @@ export {
   Footer,
   SearchBar,
   CustomFilter,
-  SearchManufacturer
+  SearchManufacturer,
+  CarCard
 }
